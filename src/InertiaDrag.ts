92169ffda888774f3895ee0a85f509de8fc2dc45
Yuji Sugiura <leader22@users.noreply.github.com>
2021-01-26 16:35:20 +0900

    Update InertiaDrag.ts

--- src/InertiaDrag.ts
+++ src/InertiaDrag.ts
@@ -13,23 +13,23 @@ interface DragEvent {
 	accumulatedY: number;
 }
 
-interface DragStartEvent extends DragEvent {
+export interface DragStartEvent extends DragEvent {
 	type: 'dragstart';
 }
 
-interface DragMoveEvent extends DragEvent {
+export interface DragMoveEvent extends DragEvent {
 	type: 'dragmove';
 }
 
-interface DragEndEvent extends DragEvent {
+export interface DragEndEvent extends DragEvent {
 	type: 'dragend';
 }
 
-interface InertiaMoveEvent extends DragEvent {
+export interface InertiaMoveEvent extends DragEvent {
 	type: 'inertiamove';
 }
 
-interface DragCancelEvent {
+export interface DragCancelEvent {
 	type: 'dragcancel';
 	dragStartX: number;
 	dragStartY: number;
