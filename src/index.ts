c922de9a6174fe916e37ec3b690d159eec98f5d7
Yuji Sugiura <y.sugiura.0316@gmail.com>
2021-01-26 16:52:59 +0900

    Fix type exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 import { InertiaDrag } from './InertiaDrag';
+export { InertiaMoveEvent, DragStartEvent, DragMoveEvent, DragEndEvent, DragCancelEvent } from './InertiaDrag';
 export default InertiaDrag;
